ab1db2fc26bf8d7fde29e60d564cd096dca87ffb
Waleed Hedar <WaleedHedar538@gmail.com>
2026-04-06 01:27:29 +0300

    اضافة التحديثات الجديدة لليوم

---
+++ SalesMamagementPL/FrmAddUdateCustomers.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace SalesManagementApp.SalesMamagementPL
+{
+    partial class FrmAddUdateCustomers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCustomerPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHouseNumber = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtGadaNumber = new System.Windows.Forms.TextBox();
+            this.txtStreetNumber = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBlockNaumber = new System.Windows.Forms.TextBox();
+            this.txtAreaName = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(570, 609);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "بيانات العميل";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnCancel.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCancel.Location = new System.Drawing.Point(9, 533);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(269, 76);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "إلغاء";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSave.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnSave.Location = new System.Drawing.Point(283, 533);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(277, 76);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "حفظ";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.txtCustomerName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtCustomerPhoneNumber);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Location = new System.Drawing.Point(10, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(550, 489);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox3.Controls.Add(this.txtNotes);
+            this.groupBox3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.groupBox3.Location = new System.Drawing.Point(22, 393);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(503, 83);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "ملاحظات";
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtNotes.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtNotes.Location = new System.Drawing.Point(11, 23);
+            this.txtNotes.Multiline = true;
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(476, 57);
+            this.txtNotes.TabIndex = 15;
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtCustomerName.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtCustomerName.Location = new System.Drawing.Point(42, 104);
+            this.txtCustomerName.Multiline = true;
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(356, 37);
+            this.txtCustomerName.TabIndex = 3;
+            this.txtCustomerName.TextChanged += new System.EventHandler(this.txtCustomerName_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(440, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "أسم العميل";
+            // 
+            // txtCustomerPhoneNumber
+            // 
+            this.txtCustomerPhoneNumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtCustomerPhoneNumber.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtCustomerPhoneNumber.Location = new System.Drawing.Point(42, 37);
+            this.txtCustomerPhoneNumber.Multiline = true;
+            this.txtCustomerPhoneNumber.Name = "txtCustomerPhoneNumber";
+            this.txtCustomerPhoneNumber.Size = new System.Drawing.Size(355, 37);
+            this.txtCustomerPhoneNumber.TabIndex = 1;
+            this.txtCustomerPhoneNumber.TextChanged += new System.EventHandler(this.txtCustomerNumber_TextChanged);
+            this.txtCustomerPhoneNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCustomerNumber_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(440, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "رقم العميل";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtHouseNumber);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtGadaNumber);
+            this.groupBox2.Controls.Add(this.txtStreetNumber);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtBlockNaumber);
+            this.groupBox2.Controls.Add(this.txtAreaName);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.groupBox2.Location = new System.Drawing.Point(22, 165);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(507, 216);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "العنوان";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(400, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 26);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "أسم المنطقة";
+            // 
+            // txtHouseNumber
+            // 
+            this.txtHouseNumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtHouseNumber.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtHouseNumber.Location = new System.Drawing.Point(20, 165);
+            this.txtHouseNumber.Multiline = true;
+            this.txtHouseNumber.Name = "txtHouseNumber";
+            this.txtHouseNumber.Size = new System.Drawing.Size(123, 37);
+            this.txtHouseNumber.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label7.Location = new System.Drawing.Point(150, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 26);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "المنزل";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label6.Location = new System.Drawing.Point(419, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 26);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "رقم الجادة";
+            // 
+            // txtGadaNumber
+            // 
+            this.txtGadaNumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtGadaNumber.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtGadaNumber.Location = new System.Drawing.Point(252, 164);
+            this.txtGadaNumber.Multiline = true;
+            this.txtGadaNumber.Name = "txtGadaNumber";
+            this.txtGadaNumber.Size = new System.Drawing.Size(123, 37);
+            this.txtGadaNumber.TabIndex = 11;
+            // 
+            // txtStreetNumber
+            // 
+            this.txtStreetNumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtStreetNumber.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtStreetNumber.Location = new System.Drawing.Point(19, 97);
+            this.txtStreetNumber.Multiline = true;
+            this.txtStreetNumber.Name = "txtStreetNumber";
+            this.txtStreetNumber.Size = new System.Drawing.Size(123, 37);
+            this.txtStreetNumber.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(149, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 26);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "الشارع";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(414, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "رقم القطعة";
+            // 
+            // txtBlockNaumber
+            // 
+            this.txtBlockNaumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtBlockNaumber.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtBlockNaumber.Location = new System.Drawing.Point(253, 96);
+            this.txtBlockNaumber.Multiline = true;
+            this.txtBlockNaumber.Name = "txtBlockNaumber";
+            this.txtBlockNaumber.Size = new System.Drawing.Size(123, 37);
+            this.txtBlockNaumber.TabIndex = 7;
+            // 
+            // txtAreaName
+            // 
+            this.txtAreaName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtAreaName.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtAreaName.Location = new System.Drawing.Point(20, 28);
+            this.txtAreaName.Multiline = true;
+            this.txtAreaName.Name = "txtAreaName";
+            this.txtAreaName.Size = new System.Drawing.Size(356, 35);
+            this.txtAreaName.TabIndex = 5;
+            // 
+            // FrmAddUdateCustomers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(594, 625);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(610, 664);
+            this.MinimumSize = new System.Drawing.Size(610, 664);
+            this.Name = "FrmAddUdateCustomers";
+            this.Text = "إضافة و تعديل بيانات العميل";
+            this.Load += new System.EventHandler(this.FrmAddUdateCustomers_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCustomerPhoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBlockNaumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAreaName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtHouseNumber;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtGadaNumber;
+        private System.Windows.Forms.TextBox txtStreetNumber;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtNotes;
+    }
+}
---
+++ SalesMamagementPL/FrmCustomers.cs
@@ -0,0 +1,170 @@
+﻿using SalesManagementApp.SalesManagementBL;
+using SalesManagementApp.SalesManagementDAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesManagementApp.SalesMamagementPL
+{
+    public partial class FrmCustomers : Form
+    {
+        public FrmCustomers()
+        {
+            InitializeComponent();
+        }
+        
+        clsCustomerBL Customer1 = new clsCustomerBL();
+
+        private void SetupDataGridView()
+        {
+            dgvAllCustomers.AllowUserToAddRows = false;
+            dgvAllCustomers.ReadOnly = true;
+            dgvAllCustomers.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvAllCustomers.MultiSelect = false;
+            dgvAllCustomers.RowHeadersVisible = false;
+
+            dgvAllCustomers.BorderStyle = BorderStyle.None;
+            dgvAllCustomers.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+
+            dgvAllCustomers.EnableHeadersVisualStyles = false;
+            dgvAllCustomers.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(45, 45, 48);
+            dgvAllCustomers.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dgvAllCustomers.ColumnHeadersDefaultCellStyle.Font = new Font("Calibri", 12, FontStyle.Bold);
+
+            dgvAllCustomers.DefaultCellStyle.BackColor = Color.White;
+            dgvAllCustomers.DefaultCellStyle.ForeColor = Color.Black;
+            dgvAllCustomers.DefaultCellStyle.SelectionBackColor = Color.FromArgb(0, 120, 215);
+            dgvAllCustomers.DefaultCellStyle.SelectionForeColor = Color.White;
+            dgvAllCustomers.DefaultCellStyle.Font = new Font("Calibri", 12);
+            dgvAllCustomers.RowTemplate.Height = 33;
+            dgvAllCustomers.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(240, 240, 240);
+
+            dgvAllCustomers.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            
+            clsCustomerBL clscustomerbL = new clsCustomerBL();
+
+            dgvAllCustomers.DataSource = clscustomerbL.GetAllCustomersBL();
+
+            // add Seq column if it doesn't exist
+            if (!dgvAllCustomers.Columns.Contains("Seq"))
+            {
+                dgvAllCustomers.Columns.Insert(0, new DataGridViewTextBoxColumn
+                {
+                    Name = "Seq",
+                    HeaderText = "م",
+                    Width = 56,
+                    ReadOnly = true
+                });
+            }
+
+            for (int i = 0; i < dgvAllCustomers.Rows.Count; i++)
+            {
+                dgvAllCustomers.Rows[i].Cells["Seq"].Value = i + 1;
+            }
+        }
+
+        private void RefereshDataGridView()
+        {
+            
+            clsCustomerBL clscustomerbL = new clsCustomerBL();
+
+            dgvAllCustomers.DataSource = clscustomerbL.GetAllCustomersBL();
+
+            
+            for (int i = 0; i < dgvAllCustomers.Rows.Count; i++)
+            {
+                dgvAllCustomers.Rows[i].Cells["Seq"].Value = i + 1;
+            }
+
+            txtSearch.Focus();
+
+
+        }
+
+        private void FrmCustomers_Load(object sender, EventArgs e)
+        {
+            cmbSearchCustomers.SelectedIndex = 0;
+            SetupDataGridView();        
+
+        }
+
+        private int GetSelectedCustomerID()
+        {
+            if (dgvAllCustomers.SelectedRows.Count > 0)
+            {
+                var row = dgvAllCustomers.SelectedRows[0];
+                int id = Convert.ToInt32(row.Cells[row.DataGridView.Columns["CustomerID"].Index].Value);
+                return id;  
+            }
+
+            return 0;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            FrmAddUdateCustomers frmAddUdateCustomers = new FrmAddUdateCustomers(0);
+            frmAddUdateCustomers.ShowDialog();
+            RefereshDataGridView();
+        }
+
+        private void btnUpdateCustomer_Click(object sender, EventArgs e)
+        {
+            if(GetSelectedCustomerID() !=0)
+            {
+                FrmAddUdateCustomers frmAddUdateCustomers = new FrmAddUdateCustomers(GetSelectedCustomerID());
+                frmAddUdateCustomers.ShowDialog();
+                RefereshDataGridView();
+            }
+            else
+                {
+                MessageBox.Show("من فضلك اختر عميل للتعديل عليه", "تنبيه", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int customerId = GetSelectedCustomerID();
+
+            if (Customer1.DeleteCustomerBL(customerId))
+            {
+                MessageBox.Show("تم حذف العميل بنجاح", "نجاح", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                RefereshDataGridView();
+
+            }
+            else
+            {
+                MessageBox.Show("حدث خطأ أثناء حذف العميل", "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnMovement_Click(object sender, EventArgs e)
+        {
+            int customerId = GetSelectedCustomerID();
+
+            if (GetSelectedCustomerID() != 0)
+            {
+                FrmCustomersMovement frmCustomersMovement = new FrmCustomersMovement(customerId);
+                frmCustomersMovement.ShowDialog();
+
+            }
+            else
+                return;
+           
+        }
+    }
+}
